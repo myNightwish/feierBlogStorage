2b5a0595b33a3caef1a4c4c39d7aec4b941ab23a
myNightwish <zml_byr@bupt.edu.cn>
2022-03-14 00:55:44 +0800

    mogai-14

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,18 @@
-
-import gulp from 'gulp';
-import cleanCSS from 'gulp-clean-css';
-import htmlmin from 'gulp-html-minifier-terser';
-import htmlclean from 'gulp-htmlclean';
-import uglify from 'gulp-uglify';
-import babel from 'gulp-babel'
-// var gulp = require('gulp');
-// var cleanCSS = require('gulp-clean-css');
-// var htmlmin = require('gulp-html-minifier-terser');
-// var htmlclean = require('gulp-htmlclean');
-// var uglify = require('gulp-uglify')
-// var babel = require('gulp-babel')
+//用到的各个插件
+var gulp = require('gulp');
+var cleanCSS = require('gulp-clean-css');
+var htmlmin = require('gulp-html-minifier-terser');
+var htmlclean = require('gulp-htmlclean');
+var fontmin = require('gulp-fontmin');
+// gulp-tester
+var terser = require('gulp-terser');
 // 压缩js
 gulp.task('compress', () =>
   gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
-    .pipe(babel({
-      presets: ['@babel/preset-env']
-    }))
-    .pipe(uglify().on('error', function (e) {
-      console.log(e)
-    }))
+    .pipe(terser())
     .pipe(gulp.dest('./public'))
 )
-// 压缩css
+//压缩css
 gulp.task('minify-css', () => {
     return gulp.src(['./public/**/*.css'])
         .pipe(cleanCSS({
@@ -30,29 +20,51 @@ gulp.task('minify-css', () => {
         }))
         .pipe(gulp.dest('./public'));
 });
-// 压缩html
+//压缩html
 gulp.task('minify-html', () => {
     return gulp.src('./public/**/*.html')
         .pipe(htmlclean())
         .pipe(htmlmin({
-            removeComments: true, 	  // 清除 html 注释
-            collapseWhitespace: true, // 压缩 html
+            removeComments: true, //清除html注释
+            collapseWhitespace: true, //压缩html
             collapseBooleanAttributes: true,
-            // 省略布尔属性的值，例如：<input checked="true"/> ==> <input />
+            //省略布尔属性的值，例如：<input checked="true"/> ==> <input />
             removeEmptyAttributes: true,
-            // 删除所有空格作属性值，例如：<input id="" /> ==> <input />
+            //删除所有空格作属性值，例如：<input id="" /> ==> <input />
             removeScriptTypeAttributes: true,
-            // 删除<script>的type="text/javascript"
+            //删除<script>的type="text/javascript"
             removeStyleLinkTypeAttributes: true,
-            // 删除<style>和<link>的 type="text/css"
-            minifyJS: true, 	// 压缩页面 JS
-            minifyCSS: true, 	// 压缩页面 CSS
-            minifyURLs: true 	// 压缩页面 URL
+            //删除<style>和<link>的 type="text/css"
+            minifyJS: true, //压缩页面 JS
+            minifyCSS: true, //压缩页面 CSS
+            minifyURLs: true  //压缩页面URL
         }))
         .pipe(gulp.dest('./public'))
 });
+//压缩字体
+function minifyFont(text, cb) {
+  gulp
+    .src('./public/fonts/*.ttf') //原字体所在目录
+    .pipe(fontmin({
+      text: text
+    }))
+    .pipe(gulp.dest('./public/fontsdest/')) //压缩后的输出目录
+    .on('end', cb);
+}
 
+gulp.task('mini-font', (cb) => {
+  var buffers = [];
+  gulp
+    .src(['./public/**/*.html']) //HTML文件所在目录请根据自身情况修改
+    .on('data', function(file) {
+      buffers.push(file.contents);
+    })
+    .on('end', function() {
+      var text = Buffer.concat(buffers).toString('utf-8');
+      minifyFont(text, cb);
+    });
+});
 // 运行gulp命令时依次执行以下任务
 gulp.task('default', gulp.parallel(
-  'compress', 'minify-css', 'minify-html'
+  'compress', 'minify-css', 'minify-html','mini-font'
 ))
